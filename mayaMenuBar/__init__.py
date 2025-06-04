cdf363046f435db3ba631fa229027464cb484089
Alexdong598 <84759871+Alexdong598@users.noreply.github.com>
2025-06-04 19:40:47 +0800

    Delete mayaMenuBar directory

--- mayaMenuBar/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Main package initialization for Maya Menu Bar."""
-from . import commands
-from . import ui
- 
-def initialize():
-    """Initialize the menu bar package."""
-    ui.create_menu()
